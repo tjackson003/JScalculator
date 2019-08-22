a9a5d742b65fc5e0c74a11b64e61b66ed4273ee1
TJ Jackson <elitefourfresh@gmail.com>
2019-08-22 02:49:18 -0400

    html and js done, understanding functionality

---
+++ assets/js/calc.js
@@ -0,0 +1,53 @@
+// VARIABLES FOR DISPLAY, BUTTONS, & OPERATIONS
+let display = document.getElementById("display");
+let buttons = document.getElementsByClassName("btn"); // buttons.length = 17
+
+let operation = "";
+
+// FUNCTIONS
+function concatOperation(button) {
+    if(button.classList.contains("operator")) {
+        if(!operation) {
+            return;
+        };
+    };
+    operation += button.dataset.value;
+    display.innerHTML = operation;
+};
+
+function callAction(button) {
+    switch(button.dataset.value) {
+        case "calculate":
+            operation = eval(operation).toString();
+            display.innerHTML = operation;
+            break;
+        case "backspace":
+            operation = operation.slice(0, -1);
+            display.innerHTML = operation;
+            break;
+        case "clear":
+            operation = "";
+            display.innerHTML = "";
+            break;
+    };
+};
+
+// CALCULATOR LOOP
+for(let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+
+    button.addEventListener("click", function() {
+        switch(button.classList.contains("action")) {
+            case true:
+                if(button.classList.contains("operator")) {
+                    concatOperation(button);
+                } else {
+                    callAction(button);
+                }
+                break;
+            case false:
+                    concatOperation(button);
+                break;
+        };
+    });
+};
